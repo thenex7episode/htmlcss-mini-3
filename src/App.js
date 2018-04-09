6a58e1fe36444fd84a49fd097e4b3b963b9ae683
William <lambermontwilliam@gmail.com>
2018-04-09 11:20:28 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className='main-nav'>
+        <div className="logo">START BOOTSTRAP</div>
+        <div clasName="links">
+        <div className="phone">Menu</div>
+        <div className="desktop">About Download Contact</div>
+        </div>
+        </nav>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
